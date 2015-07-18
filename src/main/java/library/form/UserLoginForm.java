c91736cbddaaabbf91bb421ffc3babde4446f7a0
渥美雅俊 <anny_padme@yahoo.com>
2015-07-18 21:08:39 +0900

    新規ファイル作成

--- src/main/java/library/form/UserLoginForm.java
+++ src/main/java/library/form/UserLoginForm.java
@@ -1,5 +1,22 @@
+/**
+ * 
+ */
 package library.form;
 
-public class UserLoginForm {
+import org.seasar.struts.annotation.Required;
 
+/**
+ * ログイン画面からの入力値を受け取るクラス
+ * メールアドレスとパスワードで認証する
+ * @author masayukitooyama
+ * 
+ */
+public class UserLoginForm {
+	//メールアドレス
+	@Required
+	public String mail;
+	
+	//パスワード
+	@Required
+	public String password;
 }
