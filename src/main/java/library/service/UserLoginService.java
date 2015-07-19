66f1022bfc0360a955c8a8e9fd807a511bfbf860
masayukitooyama <masayuki.dm@gmail.com>
2015-07-19 11:16:24 +0900

    ログインサービスの修正

--- src/main/java/library/service/UserLoginService.java
+++ src/main/java/library/service/UserLoginService.java
@@ -3,21 +3,31 @@ package library.service;
 import org.seasar.extension.jdbc.service.S2AbstractService;
 import org.seasar.extension.jdbc.where.SimpleWhere;
 
+import library.dto.UserInfoDto;
 import library.entity.User;
 
-public class UserLoginService extends S2AbstractService {
-
+public class UserLoginService extends S2AbstractService<User> {
+	
+	/** ユーザーの有無判定 */
 	public boolean isUserExist(String mail, String password) {
 		UserInfoDto userInfoDto = getUserInfoDto(mail);
 		return (mail.equals(userInfoDto.mail) && password.equals(userInfoDto.password));
-		return false;
 	}
-
+	/** db からユーザーを取得する */
 	private UserInfoDto getUserInfoDto(String mail) {
 		
 		User userEntity = select().where(new SimpleWhere().eq("mail", mail)).getSingleResult();
 		
-		return null;
+		UserInfoDto userInfoDto = createUserInfoDto(userEntity);
+		
+		return userInfoDto;
+	}
+	/**　ユーザー情報を記録 */
+	private UserInfoDto createUserInfoDto(User userEntity) {
+		UserInfoDto userInfoDto = new UserInfoDto();
+		userInfoDto.mail = userEntity.mail;
+		userInfoDto.password = userEntity.password;
+		return userInfoDto;
 	}
 
 }
