1520263ecf7fb1e732f5e4b90d86bbfdc9c17b22
masayukitooyama <masayuki.dm@gmail.com>
2015-07-19 14:28:33 +0900

    ログイン実装ーー

--- src/main/java/library/service/UserLoginService.java
+++ src/main/java/library/service/UserLoginService.java
@@ -8,21 +8,29 @@ import library.entity.User;
 
 public class UserLoginService extends S2AbstractService<User> {
 	
+	
+	
 	/** ユーザーの有無判定 */
 	public boolean isUserExist(String mail, String password) {
 		UserInfoDto userInfoDto = getUserInfoDto(mail);
 		return (mail.equals(userInfoDto.mail) && password.equals(userInfoDto.password));
 	}
+	
 	/** db からユーザーを取得する */
 	private UserInfoDto getUserInfoDto(String mail) {
 		
 		User userEntity = select().where(new SimpleWhere().eq("mail", mail)).getSingleResult();
-		
+		if (userEntity == null){
+			UserInfoDto illegalUserDto = new UserInfoDto();
+			illegalUserDto.mail = "illegal";
+			illegalUserDto.password = "illegal";
+			return illegalUserDto;
+		}
 		UserInfoDto userInfoDto = createUserInfoDto(userEntity);
 		
 		return userInfoDto;
 	}
-	/**　ユーザー情報を記録 */
+	/**　ユーザー情報を格納	 */
 	private UserInfoDto createUserInfoDto(User userEntity) {
 		UserInfoDto userInfoDto = new UserInfoDto();
 		userInfoDto.mail = userEntity.mail;
