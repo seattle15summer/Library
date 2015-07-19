07060027c9ed380fc28a57f08164730dfc74f413
masayukitooyama <masayuki.dm@gmail.com>
2015-07-19 22:24:11 +0900

    本の検索フォームとサービスの新規作成

---
+++ src/main/java/library/service/SearchBookService.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package library.service;
+
+import org.seasar.extension.jdbc.service.S2AbstractService;
+
+/**
+ * @author masayukitooyama
+ *
+ */
+public class SearchBookService extends S2AbstractService {
+	
+	
+
+}
